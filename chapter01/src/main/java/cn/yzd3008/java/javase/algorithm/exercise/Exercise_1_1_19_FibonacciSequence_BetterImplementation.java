d2e2ca722d03241691bfb2732e2b3b2571d5d3cf
Ted Duan <yzd3008@icloud.com>
2021-02-27 22:52:06 +0800

    A better solution to calculate Fibonacci sequence

--- chapter01/src/main/java/cn/yzd3008/java/javase/algorithm/exercise/Exercise_1_1_19_FibonacciSequence_BetterImplementation.java
+++ chapter01/src/main/java/cn/yzd3008/java/javase/algorithm/exercise/Exercise_1_1_19_FibonacciSequence_BetterImplementation.java
@@ -2,9 +2,36 @@ package cn.yzd3008.java.javase.algorithm.exercise;
 
 public class Exercise_1_1_19_FibonacciSequence_BetterImplementation {
 
-    // in one hour, without better algorithm, it will run to max N: 57
+    /*
+    * In one hour, without better algorithm, it will run to max N: 57
+     But, if we store the calculated value each time, then we can improve the performance.
+    *  */
+
+    private static final int N = 100;
+    private static long[] FibonacciCalculationResultArray = new long[N + 1];
 
     public static void main(String[] args) {
 
+        for (int i = 1; i <= N; i++) {
+            System.out.println(String.format("Fibonacci(%3d) = %d, array element value: %d", i, F(i), FibonacciCalculationResultArray[i]));
+        }
     }
+
+    private static long F(long n) {
+        if (n == 1) {
+            FibonacciCalculationResultArray[1] = 1;
+            return 1;
+        }
+
+        if (n == 2) {
+            FibonacciCalculationResultArray[2] = 1;
+            return 1;
+        }
+
+        FibonacciCalculationResultArray[(int) n] = FibonacciCalculationResultArray[(int) (n - 1)] +
+                FibonacciCalculationResultArray[(int) (n - 2)];
+
+        return FibonacciCalculationResultArray[(int) n];
+    }
+
 }
